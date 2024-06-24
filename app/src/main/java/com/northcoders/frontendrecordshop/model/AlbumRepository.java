6932a44a19544437dfbf9c31f1d305eb26534ecd
Simizzle <85163420+Simizzle@users.noreply.github.com>
2024-06-24 21:54:33 +0100

    fix: displays API data

--- app/src/main/java/com/northcoders/frontendrecordshop/model/AlbumRepository.java
+++ app/src/main/java/com/northcoders/frontendrecordshop/model/AlbumRepository.java
@@ -3,12 +3,17 @@ package com.northcoders.frontendrecordshop.model;
 import android.app.Application;
 import android.util.Log;
 import androidx.lifecycle.MutableLiveData;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.northcoders.frontendrecordshop.service.AlbumApiService;
 import com.northcoders.frontendrecordshop.service.RetrofitInstance;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AlbumRepository {
@@ -23,19 +28,24 @@ public class AlbumRepository {
     public MutableLiveData<List<Album>> getMutableLiveData(){
         AlbumApiService albumApiService = RetrofitInstance.getService();
 
-        Call<List<Album>> call = albumApiService.getAlbums();
+//        Call<List<Album>> call = albumApiService.getAlbums();
+        Call<ResponseBody> call = albumApiService.getRawJson();
 
-        call.enqueue(new Callback<List<Album>>(){
+        call.enqueue(new Callback<ResponseBody>() {
             @Override
-            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
-                mutableLiveData.setValue(response.body());
-
-
+            public void onResponse(retrofit2.Call<ResponseBody> call, Response<ResponseBody> response) {
+                Gson gson = new Gson();
+                Type listType = new TypeToken<ArrayList<Album>>(){}.getType();
+                ArrayList<Album> list = gson.fromJson(response.body().charStream(), listType);
+                Log.i("album List log", response.message());
+                mutableLiveData.setValue(list);
             }
 
             @Override
-            public void onFailure(Call<List<Album>> call, Throwable throwable) {
-
+            public void onFailure(Call<ResponseBody> call, Throwable throwable) {
+                // Handle failure case
+                Log.i("album List log", throwable.getMessage());
+                throwable.printStackTrace();
             }
         });
         return mutableLiveData;
